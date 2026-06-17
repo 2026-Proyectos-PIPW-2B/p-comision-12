e53cc096821694b28b3bd8ab2507ad16cb2ebeea
Maximiliano Riquelme Vera <m.riquelme.vera@hotmail.com>
2026-06-17 16:34:27 -0300

    Creacion categoria wip

--- js/creacion_etiquetas.js
+++ js/creacion_etiquetas.js
@@ -18,12 +18,108 @@ const inputDescripcionCrearEtiqueta = document.getElementById(
 );
 const buttonCrearEtiqueta = document.getElementById("buttonCrearEtiqueta");
 
+// Constantes Form Edicion Categoria
+const formEdicionCategoria = document.getElementById("formEdicionCategoria");
+const inputNameEdicionCategoria = document.getElementById(
+  "inputNameEdicionCategoria",
+);
+const inputDescripcionEdicionCategoria = document.getElementById(
+  "inputDescripcionEdicionCategoria",
+);
+const buttonEdicionCategoria = document.getElementById(
+  "buttonEdicionCategoria",
+);
+
+// Constantes Form Edicion Etiqueta
+const formEdicionEtiqueta = document.getElementById("formEdicionEtiqueta");
+const inputNameEdicionEtiqueta = document.getElementById(
+  "inputNameEdicionEtiqueta",
+);
+const inputDescripcionEdicionEtiqueta = document.getElementById(
+  "inputDescripcionEdicionEtiqueta",
+);
+const buttonEdicionEtiqueta = document.getElementById("buttonEdicionEtiqueta");
+
 // localStorage
 let categorias = JSON.parse(localStorage.getItem("categorias")) || [];
 let etiquetas = JSON.parse(localStorage.getItem("etiquetas")) || [];
 
 window.addEventListener("load", function () {
-  inicializar();
+  inicializarCrearCategoria();
 });
 
-function inicializar() {}
+function inicializarCrearCategoria() {
+  formCrearCategoria.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    limpiarValidacion();
+
+    if (validarDatos(inputNameCrearCategoria, inputDescripcionCrearCategoria)) {
+      formCrearCategoria.reset();
+      limpiarValidacion();
+    }
+  });
+}
+
+function limpiarValidacion() {
+  const inputs = document.querySelectorAll(
+    ".form-control, .form-select, .form-check-input",
+  );
+  for (const input of inputs) {
+    input.classList.remove("is-invalid");
+    input.classList.remove("is-valid");
+  }
+}
+
+function validarDatos(inputNombre, inputDescripcion) {
+  let validacion = true;
+
+  if (!validarNombre(inputNombre)) {
+    validacion = false;
+  }
+  if (!validarDescripcion(inputDescripcion)) {
+    validacion = false;
+  }
+
+  return validacion;
+}
+
+function mostrarMensaje(input, validacion) {
+  if (validacion) {
+    input.classList.remove("is-invalid");
+    input.classList.add("is-valid");
+  } else {
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
+  }
+}
+
+function validarNombre(inputNombre) {
+  let validacion = true;
+  let mensaje = "";
+
+  if (validator.isEmpty(inputNombre.value.trim())) {
+    validacion = false;
+  } else if (!validator.isLength(inputNombre.value.trim(), { min: 3 })) {
+    validacion = false;
+  }
+
+  mostrarMensaje(inputNombre, validacion);
+  return validacion;
+}
+
+function validarDescripcion(inputDescripcion) {
+  let validacion = true;
+  let mensaje = "";
+
+  if (validator.isEmpty(inputDescripcion.value.trim())) {
+    validacion = false;
+  } else if (
+    !validator.isLength(inputDescripcion.value.trim(), { min: 1, max: 200 })
+  ) {
+    validacion = false;
+  }
+
+  mostrarMensaje(inputDescripcion, validacion);
+  return validacion;
+}

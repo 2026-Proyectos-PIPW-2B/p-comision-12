33e223a377ea81917335fbd99cbd652ddb5479e4
dario pokorny <dario.pokorny@gmail.com>
2026-06-30 16:52:42 -0300

    redirecciona al inicio de sesion si no hay cuenta iniciada

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import { consultar_sesion } from "../js/gestores/gestor_sesion.js";
+
+window.addEventListener("load", function() {
+    redireccionado();
+});
+
+function redireccionado() {
+    const sesion = consultar_sesion();
+
+    if (sesion === false) {
+        window.location.href = "../html/inicioSesion.html";
+    }
+}
